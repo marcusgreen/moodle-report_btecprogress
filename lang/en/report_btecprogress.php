9c66f97778af7f5be8b6d5ce4cacdcd48fc75fe2
unknown <marcusavgreen@gmail.com>
2015-08-02 22:37:57 +0100

    added string for empty grade table

--- lang/en/report_btecprogress.php
+++ lang/en/report_btecprogress.php
@@ -26,5 +26,6 @@ $string['pluginname'] = 'BTEC progress';
 $string['title'] = 'BTEC progress report';
 $string['progresstblheader']='progresstbleheader';
 $string['key']="Key:!= New Submission,N=Not Submitted, A=Achieved,R=Referred";
+$string['noassigns']='No assignments found, does the course contain BTEC graded assignments?';
 
 
